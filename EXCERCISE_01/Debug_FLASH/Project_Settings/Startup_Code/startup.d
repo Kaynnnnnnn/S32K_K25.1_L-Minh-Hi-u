eb0e17c2d8a0cd2362315406021d811ddfa01153
Kaynnnnnnn <hieuhieu29092004@gmail.com>
2025-09-21 22:30:19 +0700

    Exercise_01

---
+++ EXCERCISE_01/Debug_FLASH/Project_Settings/Startup_Code/startup.d
@@ -0,0 +1,14 @@
+Project_Settings/Startup_Code/startup.o: \
+ ../Project_Settings/Startup_Code/startup.c \
+ D\:/EXCERCISE_01/include/startup.h \
+ D\:/EXCERCISE_01/include/device_registers.h \
+ D\:/EXCERCISE_01/include/s32_core_cm4.h \
+ D\:/EXCERCISE_01/include/S32K144.h \
+ D\:/EXCERCISE_01/include/S32K144_features.h \
+ D\:/EXCERCISE_01/include/devassert.h
+D\:/EXCERCISE_01/include/startup.h:
+D\:/EXCERCISE_01/include/device_registers.h:
+D\:/EXCERCISE_01/include/s32_core_cm4.h:
+D\:/EXCERCISE_01/include/S32K144.h:
+D\:/EXCERCISE_01/include/S32K144_features.h:
+D\:/EXCERCISE_01/include/devassert.h:
---
+++ EXCERCISE_01/Debug_RAM/src/main.d
@@ -0,0 +1,2 @@
+src/main.o: ../src/main.c D\:/EXCERCISE_01/include/S32K144.h
+D\:/EXCERCISE_01/include/S32K144.h:
